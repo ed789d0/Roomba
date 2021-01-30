cd13d5c97bff2002d2e52645a8af1a027102772a
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2021-01-30 14:30:18 -0500

    Update RoombaArduinoVirtualWallEASY.ino

--- Arduino Code/RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
+++ Arduino Code/RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
@@ -12,7 +12,8 @@
   Uses "A Multi-Protocol Infrared Remote Library for the Arduino":
   http://www.arcfn.com/2009/08/multi-protocol-infrared-remote-library.html
 */
-
+#include <avr/sleep.h>
+#include <avr/power.h>
 #include <IRremote.h>
 
 // Initiate IRremote on Pin 3.
@@ -20,7 +21,12 @@ IRsend irsend;
 
 void setup()
 {
-  irsend.enableIROut(38);//Lib function
+  irsend.enableIROut(38); // Lib function
+  // turns off stuff that's not needed for the circuit, saving power
+  ADCSRA = 0;
+  power_adc_disable();
+  power_spi_disable();
+  power_twi_disable();
 }
 
 void loop()
